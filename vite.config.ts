cd8e64609992b7d3f9d0508a84e5a1058229f4a4
wu rang <rangwu@gitlab.cn>
2025-11-03 10:04:43 +0800

    feat: update build package filename

--- vite.config.ts
+++ vite.config.ts
@@ -84,7 +84,7 @@ export default defineConfig({
                     zipPack({
                         inDir: './dist',
                         outDir: './',
-                        outFileName: 'package.zip'
+                        outFileName: 'siyuan-github-publish-plugin.zip'
                     })
                 ])
             ],
